09283bc94b6c3d6c54f8e4adcbcfdd6e60b96605
Gonzalo Canteli Diaz-Madroñero <7903359@alumnos.ufv.es>
2021-09-21 21:03:33 +0200

    first commit

---
+++ Persona.java
@@ -0,0 +1,39 @@
+package Persona;
+
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private int dnisinletra;
+    private int edad;
+
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellidos(){
+        return apellidos;
+    }
+    public int getDnisinletra(){
+        return dnisinletra;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setNombre(String nom){
+        this.nombre = nom;
+    }
+    public void setApellidos(String apel){
+        this.apellidos= apel;
+    }
+    public void setDnisinletra(int dni){
+        this.dnisinletra = dni;
+    }
+    public void setEdad(int ed){
+        this.edad = ed;
+    }
+
+
+
+
+
+
+}
---
+++ main.java
@@ -0,0 +1,19 @@
+import Persona.Persona;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        Persona p = new Persona();
+        System.out.println("Escribe tu nombre: ");
+        p.setNombre(sc.nextLine());
+        System.out.println("Escribe tus apellidos: ");
+        p.setApellidos(sc.nextLine());
+        System.out.println("Escribe tu dni sin letra: ");
+        p.setDnisinletra(Integer.parseInt(sc.nextLine()));
+        System.out.println("Escribe tu edad: ");
+        p.setEdad(Integer.parseInt(sc.nextLine()));
+
+        System.out.println("Hola "+ p.getNombre()+" "+p.getApellidos()+" con DNI:  "+String.valueOf(p.getDnisinletra())+" y "+String.valueOf(p.getEdad())+" años");
+
+    }
+}
